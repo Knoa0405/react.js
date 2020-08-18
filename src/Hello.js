ea198250e08e846c8ba9458fa4070b2e9f7281bc
wowgod9504 <mike1124@naver.com>
2020-08-18 18:32:19 +0900

    initial commit with react.js

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hello({color, name}) {
+    const style = {
+        color
+    }
+return <div style={style}>안녕하세요 {name}</div>
+}
+
+
+Hello.defaultProps = {
+    name : "이름없음"
+};
+export default Hello;
