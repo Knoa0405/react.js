94dd38c9fadb66b3ab64a76022223c9b417c73f4
wowgod9504 <mike1124@naver.com>
2020-08-19 23:16:07 +0900

    practice react.js - 1 / hooks useState / useRef /useEffect etc..

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Hello({color, name}) {
+function Hello({color, name, isSpecial }) {
     const style = {
         color
     }
-return <div style={style}>안녕하세요 {name}</div>
+    return (
+        <div style={style}>
+            <b>{isSpecial ? "스폐셜" : "낫스폐셜" }</b>
+            안녕하세요
+        </div>
+)
 }
 
 
