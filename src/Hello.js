72e6714df884c6cee72698ccb283d1c2f120676e
wowgod9504 <mike1124@naver.com>
2020-08-28 22:23:23 +0900

    🎨practice immer & Class Component

--- src/Hello.js
+++ src/Hello.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { component } from "react";
 
-function Hello({color, name, isSpecial }) {
-    const style = {
-        color
+
+class Hello extends component {
+    static defaultProps = {
+        name : "이름없음"
     }
-    return (
-        <div style={style}>
-            <b>{isSpecial ? "스폐셜" : "낫스폐셜" }</b>
-            안녕하세요
-        </div>
-)
+    render() {
+        const { name, color, isSpecial } = this.props;
+        return (
+            <div style={{color}}>
+                <b>{isSpecial ? "스폐셜" : "낫스폐셜" }</b>
+                안녕하세요{name}
+            </div>
+        )
+    }
 }
+// function Hello({color, name, isSpecial }) {
+//     const style = {
+//         color
+//     }
+//     return (
+//         <div style={style}>
+//             <b>{isSpecial ? "스폐셜" : "낫스폐셜" }</b>
+//             안녕하세요
+//         </div>
+// )
+// }
 
 
-Hello.defaultProps = {
-    name : "이름없음"
-};
+// Hello.defaultProps = {
+//     name : "이름없음"
+// };
 export default Hello;
